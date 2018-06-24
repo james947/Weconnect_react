61336e3ef46b731faf23eff6ecf27091b1691f3d
james947 <jmuriuki947@gmail.com>
2018-06-24 08:45:40 +0300

    [ADD] SignupPage and LogingPage to Routes

--- src/route.jsx
+++ src/route.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
-import SignupPage from './components/SignupPage/SignupPage';
+import SignupPage from './containers/SignupPage';
+import LoginPage from './containers/LoginPage';
 import Businesses from './components/Businesses';
-import LoginPage from './components/LoginPage/index';
-
+import RegisterBusiness from './components/RegisterBusiness/BusinessForm';
 
 const Routes = () =>
   (
@@ -13,8 +13,8 @@ const Routes = () =>
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/signup" component={SignupPage} />
       <Route exact path="/businesses" component={Businesses} />
+      <Route exact path="/add_business" component={RegisterBusiness} />
     </div>
   );
 
 export default Routes;
-
