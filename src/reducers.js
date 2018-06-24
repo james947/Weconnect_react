2c39eb895d9dd932d659fe93a4ac468f7a93a2b6
james947 <jmuriuki947@gmail.com>
2018-06-24 08:44:21 +0300

    [ADD] user reducer to rootReducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
-import fetchBusinessReducer from './redux/reducers';
+import fetchBusinessReducer from './redux/reducers/reducers';
+import user from './redux/reducers/userReducer';
 
 const rootReducer = combineReducers({
-  business: fetchBusinessReducer
+  business: fetchBusinessReducer,
+  user
+
 });
 
 export default rootReducer;
