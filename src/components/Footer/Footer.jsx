ac02fa07e847a19a7d6f1b4ee741983e338502dd
james947 <jmuriuki947@gmail.com>
2018-07-04 23:05:43 +0300

    [Feature #158827222] Add footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
 
 const Footer = () => (
 
   <footer className="text-center">
     <p>Developed by James Muriuki</p>
-    <small>&copy; Copyright 2018 Terms and Conditions Apply</small>
+    <small>&copy; <Link className="footerCss" to="/terms">Copyright 2018 Terms and Conditions Apply</Link></small>
   </footer>
 
 );
