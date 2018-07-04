7b3e2c2e74a580808193a348c04e740d74c35c29
james947 <jmuriuki947@gmail.com>
2018-07-04 22:56:26 +0300

    [UPDATE] routes file

--- src/route.jsx
+++ src/route.jsx
@@ -4,7 +4,12 @@ import LandingPage from './components/LandingPage/LandingPage';
 import SignupPage from './containers/SignupPage';
 import LoginPage from './containers/LoginPage';
 import Businesses from './components/Businesses';
-import RegisterBusiness from './components/RegisterBusiness/BusinessForm';
+import ForgotPassword from './containers/ForgotPassword';
+import RegisterPage from './containers/RegisterBusiness';
+import { TermsConditions } from './components/TermsAndConditions';
+import ProfilePage from './containers/BusinessProfile/index';
+import EditPage from './containers/EditForm';
+import PasswordPage from './containers/PasswordPage';
 
 const Routes = () =>
   (
@@ -12,8 +17,13 @@ const Routes = () =>
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/signup" component={SignupPage} />
+      <Route exact path="/forgot-password" component={ForgotPassword} />
       <Route exact path="/businesses" component={Businesses} />
-      <Route exact path="/add_business" component={RegisterBusiness} />
+      <Route exact path="/add-business" component={RegisterPage} />
+      <Route exact path="/edit-business" component={EditPage} />
+      <Route exact path="/terms" component={TermsConditions} />
+      <Route exact path="/businesses/:id" component={ProfilePage} />
+      <Route exact path="change-password" component={PasswordPage} />
     </div>
   );
 
