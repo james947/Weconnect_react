2449256ae838bff84df1611d9a3ff2b956d8f450
james947 <jmuriuki947@gmail.com>
2018-07-04 22:58:00 +0300

    [FIX] local storage token error

--- src/apiRequests.js
+++ src/apiRequests.js
@@ -1,31 +1,50 @@
 import axios from 'axios';
 
+
 export const instance = axios.create({
   baseURL: 'https://weconnect-v3.herokuapp.com/api/v1',
   timeout: 20000,
   headers: {
-    Accept: 'application/json',
-    ContentType: 'application/json',
+    "Accept": 'application/json',
+    "ContentType": 'application/json',
+    'x-access-token': localStorage.getItem('auth_token')
   }
 });
 
 
-instance.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem('auth_token');
-  if (accessToken) {
-    config.headers.Authorization = { accessToken };
-  }
-  return config;
-});
-
-
-const apiRequest = {
+export const apiRequest = {
   user: {
     login: (userData) => instance.post('/auth/login', userData)
-      .then(res => res.data.user),
+      .then(res => res.data),
     signup: (userData) => instance.post('/auth/register', userData)
+      .then(res => res.data),
+    logout: () => instance.delete('/auth/logout')
+      .then(res => res.data),
+    reset: (userData) => instance.post('/auth/reset-password', userData)
+      .then(res => res.data),
+    change: (userData) => instance.post('/auth/change-password', userData)
       .then(res => res.data)
-  }
-};
+  },
 
+  business: {
+    create: (newBusiness) => instance.post('/businesses', newBusiness)
+      .then(response => response.data),
+    fetch: (params) => instance.get(`/businesses/${params.id}`)
+      .then(response => response.data),
+    edit: (updatedBusiness, businessId) => instance.put(`/businesses/${businessId}`, updatedBusiness)
+      .then(response => response.data),
+    delete: (businessId) => instance.delete(`/businesses/${businessId}`)
+      .then(response => response.data),
+    reviews: (businessId) => instance.get(`/businesses/${businessId}/reviews`)
+      .then(response => response.data),
+    postReview: (businessId, review) => {
+      console.log(localStorage.getItem('auth_token'));
+      return instance.post(`/businesses/${businessId}/reviews`, review)
+        .then(response => response.data);
+    }
+
+  }
+
+};
 export default apiRequest;
+
