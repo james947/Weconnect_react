de30bb6486537ae39b26db7388296d0a59e9ca46
james947 <jmuriuki947@gmail.com>
2018-07-04 23:03:31 +0300

    [Update] landing page with business proposition

--- src/components/LandingBody/LandingBody.jsx
+++ src/components/LandingBody/LandingBody.jsx
@@ -8,6 +8,7 @@ const LandingBody = () => (
     <div className="row">
       <div className="text-align-center offset-md-2 col-md-8">
         <h1>WELCOME TO WECONNECT</h1>
+        <h5>WEConnect is a platform that enables you to register your business and connect with other businesses</h5>
       </div>
       <div id="landingPage" className="btn-group actions">
         <Link to="businesses"className="btn btn-primary btn-lg">Try now</Link>
