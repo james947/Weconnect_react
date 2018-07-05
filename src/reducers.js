d6ced60342785e40938c0cb006a0b9fd0b987a75
james947 <jmuriuki947@gmail.com>
2018-07-05 19:17:09 +0300

    [FIX] business update reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
-import fetchBusinessReducer from './redux/reducers/reducers';
+import businesses from './redux/reducers/reducers';
 import fetchReviews from './redux/reducers/reviews';
 import user from './redux/reducers/userReducer';
 
 
 const rootReducer = combineReducers({
-  business: fetchBusinessReducer,
+  businesses,
   user,
   fetchReviews
 
