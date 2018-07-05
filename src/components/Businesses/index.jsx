eabe9da70ad92dc13ff4617f14be3f20fe5a65df
james947 <jmuriuki947@gmail.com>
2018-07-05 04:27:36 +0300

    [Finishes #158829075] implements a dropdown for filtering a business by location and category

--- src/components/Businesses/index.jsx
+++ src/components/Businesses/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FetchBusinesses from '../../containers/FetchBusinesses';
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
@@ -6,7 +7,24 @@ import NavBar from '../NavBar/NavBar';
 const Businesses = () => (
   <div>
     <NavBar />
-    <FetchBusinesses />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2">
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                filter by
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <Link className="dropdown-item" to="#">Category</Link>
+              <Link className="dropdown-item" to="#">Location</Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-10">
+          <FetchBusinesses />
+        </div>
+      </div>
+    </div>
     <Footer />
   </div>
 );
