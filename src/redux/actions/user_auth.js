47ef7f1ed9c04d4093e335adec2499be15388357
james947 <jmuriuki947@gmail.com>
2018-07-05 04:56:28 +0300

    [Chore] users redux actions

---
+++ src/redux/actions/user_auth.js
@@ -0,0 +1,40 @@
+
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../../types';
+import { apiRequest } from '../../apiRequests';
+
+export const userLoggedIn = user => ({
+  type: USER_LOGGED_IN,
+  user
+});
+
+export const userLoggedOut = () => ({
+  type: USER_LOGGED_OUT
+});
+
+
+export const userSignupRequest = userData => () =>
+  apiRequest.user.signup(userData).then(res => res);
+
+
+export const userLoginRequest = userData => (dispatch) =>
+  apiRequest.user.login(userData).then(user => {
+    const { token } = user;
+    localStorage.setItem('auth_token', token);
+    dispatch(userLoggedIn(user));
+  });
+
+
+export const userLogoutRequest = (token) => (dispatch) => {
+  localStorage.removeItem('auth_token');
+  apiRequest.user.logout().then(res => res);
+  dispatch(userLoggedOut());
+};
+
+// user logout actionCreator
+
+export const resetPassword = userData => (dispatch) =>
+  apiRequest.user.reset(userData).then(res => res);
+
+
+export const changePassword = userData => (dispatch) =>
+  apiRequest.user.change(userData).then(res => res);
