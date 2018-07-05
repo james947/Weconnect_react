1be77eb5ec726739bc7cc230175d02d7a8e523b6
james947 <jmuriuki947@gmail.com>
2018-07-05 04:35:25 +0300

    [Finishes #158829137] implements terms and condition page

---
+++ src/components/TermsAndConditions/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export const TermsConditions = () => (
+  <div className="jumbotron text-center">
+    <h2>Terms and Conditions Coming Soon</h2>
+  </div>
+);
+export default TermsConditions;
