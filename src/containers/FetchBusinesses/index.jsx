f8774da3df27ec61d62a85737fae32a5480cb441
james947 <jmuriuki947@gmail.com>
2018-07-05 04:55:20 +0300

    [ADD] pagination feautre

--- src/containers/FetchBusinesses/index.jsx
+++ src/containers/FetchBusinesses/index.jsx
@@ -3,32 +3,52 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
-import getBusinesses from '../../redux/actions/actions';
+import { getBusinesses, getBusiness } from '../../redux/actions/actions';
 
 class FetchBusinesses extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      pages: [],
+      page: 1,
+      total_pages: 2,
+      search: {
+        q: "",
+        category: "",
+        location: "",
+      },
+      searching: null
     };
   }
 
-  componentDidMount() {
-    console.log(this.props.businesses);
+
+  componentWillMount() {
     axios.get('https://weconnect-v3.herokuapp.com/api/v1/businesses')
       .then(response => this.props.getBusinesses(response));
   }
 
+  handleSearch = e => {
+    this.setState({
+      search: { ...this.state.search, [e.target.name]: e.target.value },
+    }, () => {
+      axios.get(`https://weconnect-v3.herokuapp.com/api/v1/businesses?q=${this.state.search.q}`)
+        .then(response => this.props.getBusiness(response));
+    });
+  }
+
   render() {
     const renderBusinesses = this.props.businesses.map(business => (
-      <div className="card col-md-4" key={business.id}>
-        <div className="card-body">
-          <h5>{business.businessname}</h5>
-          <p>{business.description}</p>
-          <p>{business.category}</p>
-          <p>{business.location}</p>
-          <p>{business.created_at}</p>
-          <Link to="/signup" className="btn btn-success btn-sm">View</Link>
+      <div className="card-deck">
+        <div className="card" key={business.id}>
+          <div className="card-header bg-transparent">{business.businessname}</div>
+          <div className="card-body">
+            <p className="card-text">Category {business.category}</p>
+            <p className="card-text">Description {business.description}</p>
+            <p className="card-text">Location {business.location}</p>
+          </div>
+          <div className="card-footer">
+            <Link to={`businesses/${business.id}`} className="btn btn-success btn-sm float-right">View</Link>
+          </div>
         </div>
       </div>
 
@@ -36,8 +56,15 @@ class FetchBusinesses extends Component {
     ));
 
     return (
-      <div className="container customCard">
+      <div className="wrapper customCard">
         <div className="row">
+          <div className="col-md-10">
+            <form className=""role="search">
+              <div className="form-group">
+                <input type="text" className="form-control" name="q" onSubmit={this.handleSearch} placeholder="Search"/>
+              </div>
+            </form>
+          </div>
           { renderBusinesses }
         </div>
       </div>
@@ -48,11 +75,12 @@ class FetchBusinesses extends Component {
 // takes a piece of apps state and passes it as a prop to the component
 const mapStateToProps = state => ({
   businesses: state.business.businesses
+
 });
 
 //Dipspatches actions to be used as props in this component
 const mapDispatchToProps = dispatch => bindActionCreators({
-  getBusinesses
+  getBusinesses, getBusiness
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(FetchBusinesses);
