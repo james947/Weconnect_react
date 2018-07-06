ff83129993b2275bdacbd0234fc24f4795e86838
james947 <jmuriuki947@gmail.com>
2018-07-06 12:29:10 +0300

    [ADD] test landing body

---
+++ src/tests/LandingBody.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {shallow} from "enzyme";
+import LandingBody from '../components/LandingBody/LandingBody';
+
+describe("landing body", () => {
+    it("it should render button", () => {
+        expect (
+            shallow(<LandingBody/>).find("Link.btn").exists()).toBe(true);
+        
+    })
+})
