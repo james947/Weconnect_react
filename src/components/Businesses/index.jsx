6f674473954c4c37937f2d467be2fea936b4c043
james947 <jmuriuki947@gmail.com>
2018-07-07 21:39:25 +0300

    [DELETE] search bar component

--- src/components/Businesses/index.jsx
+++ src/components/Businesses/index.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import FetchBusinesses from '../../containers/FetchBusinesses';
 import Footer from '../Footer/Footer';
 import NavBar from '../NavBar/NavBar';
+import SearchBusiness from '../../containers/SearchBusiness';
 
 const Businesses = () => (
   <div>
     <NavBar />
     <div className="container">
-      <div className="row">
-        <div className="col-md-2">
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                filter by
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <Link className="dropdown-item" to="#">Category</Link>
-              <Link className="dropdown-item" to="#">Location</Link>
-            </div>
-          </div>
-        </div>
         <div className="col-md-10">
+        <SearchBusiness/>
           <FetchBusinesses />
         </div>
-      </div>
     </div>
     <Footer />
   </div>
