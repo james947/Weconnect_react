eeb0b278b0de348ef2e9ac42cbd34304b80417cb
james947 <jmuriuki947@gmail.com>
2018-07-07 22:44:45 +0300

    [FIX] tests render business components

--- src/tests/components/Businesses.test.js
+++ src/tests/components/Businesses.test.js
@@ -2,10 +2,10 @@ import React from "react";
 import {shallow} from "enzyme";
 import Businesses from "../../components/Businesses";
 
-describe("Footer section", () => {
-    it("it should render businesses", () => {
+describe("businesses", () => {
+    it("it should render a  link", () => {
         expect (
-            shallow(<Businesses/>).find("Link.dropdown-item").exists()).toBe(true);
+            shallow(<Businesses/>).find("div.col-md-10").exists()).toBe(true);
         
     })
 })
