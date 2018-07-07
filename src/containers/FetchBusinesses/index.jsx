995856929d66deb7fa00ba602a3020ae234e82a8
james947 <jmuriuki947@gmail.com>
2018-07-07 21:49:59 +0300

    [FIX] delete white space

--- src/containers/FetchBusinesses/index.jsx
+++ src/containers/FetchBusinesses/index.jsx
@@ -1,44 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
-import { getBusinesses, getBusiness } from '../../redux/actions/actions';
+import { getBusinesses } from '../../redux/actions/business';
+import SearchBusiness from '../SearchBusiness';
 
 class FetchBusinesses extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pages: [],
-      page: 1,
-      total_pages: 2,
-      search: {
-        q: "",
-        category: "",
-        location: "",
-      },
-      searching: null
     };
-  }
 
+  }
 
   componentWillMount() {
-    axios.get('https://weconnect-v3.herokuapp.com/api/v1/businesses')
-      .then(response => this.props.getBusinesses(response));
+    this.props.getBusinesses();
   }
-
-  handleSearch = e => {
-    e.preventDefault;
-    this.setState({
-      search: { ...this.state.search, [e.target.name]: e.target.value },
-    }, () => {
-      axios.get(`https://weconnect-v3.herokuapp.com/api/v1/businesses?q=${this.state.search.q}`)
-        .then(response => this.props.getBusiness(response));
-    });
-  }
+  
 
   render() {
-    const renderBusinesses = this.props.businesses.map(business => (
+    const { businesses, filteredBusinesses } = this.props;
+    let container;
+    if(filteredBusinesses.length === 0){
+      container = businesses
+    }else{
+      container =  filteredBusinesses
+    }
+    
+    const renderBusinesses = container.map(business => (
       <div className="card-deck">
         <div className="card" key={business.id}>
           <div className="card-header bg-transparent">{business.businessname}</div>
@@ -59,13 +48,6 @@ class FetchBusinesses extends Component {
     return (
       <div className="wrapper customCard">
         <div className="row">
-          <div className="col-md-10">
-            <form className=""role="search">
-              <div className="form-group">
-                <input type="text" className="form-control" name="q" onSubmit={this.handleSearch} placeholder="Search"/>
-              </div>
-            </form>
-          </div>
           { renderBusinesses }
         </div>
       </div>
@@ -75,13 +57,14 @@ class FetchBusinesses extends Component {
 
 // takes a piece of apps state and passes it as a prop to the component
 const mapStateToProps = state => ({
-  businesses: state.businesses
+  businesses: state.businesses,
+  filteredBusinesses : state.filteredBusinesses
 
 });
 
 //Dipspatches actions to be used as props in this component
 const mapDispatchToProps = dispatch => bindActionCreators({
-  getBusinesses, getBusiness
+  getBusinesses
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(FetchBusinesses);
