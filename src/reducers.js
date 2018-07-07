d5069e5b971fc1920c1da8c43b8b3c840fe70dea
james947 <jmuriuki947@gmail.com>
2018-07-07 21:31:45 +0300

    [ADD] filtered business reducer

--- src/reducers.js
+++ src/reducers.js
@@ -2,12 +2,15 @@ import { combineReducers } from 'redux';
 import businesses from './redux/reducers/reducers';
 import fetchReviews from './redux/reducers/reviews';
 import user from './redux/reducers/userReducer';
+import filteredBusinesses from './redux/reducers/filtersReducer';
 
 
 const rootReducer = combineReducers({
   businesses,
   user,
-  fetchReviews
+  fetchReviews,
+  filteredBusinesses
+
 
 
 });
