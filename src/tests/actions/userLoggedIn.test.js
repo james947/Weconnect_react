67022776d2c3f09292c05debe592cd7419ea8e44
james947 <jmuriuki947@gmail.com>
2018-07-08 16:02:37 +0300

    FIX userLoggedIn test description

--- src/tests/actions/userLoggedIn.test.js
+++ src/tests/actions/userLoggedIn.test.js
@@ -3,7 +3,7 @@ import * as types from '../../types'
 
 
 describe('userLoggedIn', () => {
-    it('should create an userLoggedin', () => {
+    it('should create an action userLoggedOut', () => {
       const user = 'token'
       const expectedAction = {
         type: types.USER_LOGGED_IN,
