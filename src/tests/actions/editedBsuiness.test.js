29bd92bdbc9435c647c2e3e1e84b948176b53dea
james947 <jmuriuki947@gmail.com>
2018-07-09 05:27:55 +0300

    ADD action test for edit business

---
+++ src/tests/actions/editedBsuiness.test.js
@@ -0,0 +1,15 @@
+import * as actions from '../../redux/actions/business';
+import * as types from '../../types'
+
+
+describe('editedBusiness', () => {
+    it('should return the business edited', () => {
+      const business = '{results[{Businessname: mama mboga}]}'
+      const expectedAction = {
+        type: types.EDIT_BUSINESS,
+        business
+        
+      }
+      expect(actions.editedBusiness(business)).toEqual(expectedAction)
+    })
+  })
