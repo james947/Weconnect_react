1af47e939ec800ce040fd783428d29d08a18629b
james947 <jmuriuki947@gmail.com>
2018-07-09 05:25:41 +0300

    ADD action test for newReview

---
+++ src/tests/actions/newReview.test.js
@@ -0,0 +1,15 @@
+import * as actions from '../../redux/actions/business';
+import * as types from '../../types'
+
+
+describe('newReviews', () => {
+    it('should retrun a posted review', () => {
+      const reviews = "{title: your app is awesome}"
+      const expectedAction = {
+        type: types.NEW_REVIEW,
+        reviews
+
+      }
+      expect(actions.newReview(reviews)).toEqual(expectedAction)
+    })
+  })
