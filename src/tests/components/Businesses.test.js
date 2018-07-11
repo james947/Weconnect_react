7bef8530f73ddb837fff8c71e0fec42ccdf4f0e4
james947 <jmuriuki947@gmail.com>
2018-07-11 12:23:40 +0300

    ADD component business test

--- src/tests/components/Businesses.test.js
+++ src/tests/components/Businesses.test.js
@@ -3,9 +3,14 @@ import {shallow} from "enzyme";
 import Businesses from "../../components/Businesses";
 
 describe("<Businesses/>", () => {
-    it("it should render a  link", () => {
+    it("it should render a  div", () => {
         expect (
             shallow(<Businesses/>).find("div.col-md-10").exists()).toBe(true);
+        
+    })
+    it("it should be wrapped in a container", () => {
+        expect (
+            shallow(<Businesses/>).find("div.container").exists()).toBe(true);
         
     })
 })
