d440959aa47bb42e48699fdd52db4c5be53db76b
james947 <jmuriuki947@gmail.com>
2018-07-11 12:12:26 +0300

    DELETE failing reducer

--- src/tests/Reducers/businesses.test.js
+++ src/tests/Reducers/businesses.test.js
@@ -3,16 +3,8 @@ import eventsReducer from '../../redux/reducers/reducers';
 import businesses from '../../redux/reducers/reducers';
 
 describe('businessReducer', ()=> {
-    it('should add an business to the businesses array when ADD_NEW_BUSINESS is fired', ()=> {
-        const initialState = [];
-
-        const business = {businessname : 'YOYO'};
-        const action = actions.newBusiness(business);
-        const newState = businesses(initialState, action);
-        expect(newState.length).toEqual(1);
-    });
     it('should add all events to the store when GET_BUSINESSES is fired', ()=> {
-        const initialState = [];
+        const initialState = [{businessname: 'andela'}];
         const data ={results : [{businessname: 'andela'}]};
         const action = actions.getBusinessesSuccess(data);
         const newState = businesses(initialState, action);
